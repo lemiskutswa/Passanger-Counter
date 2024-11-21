d99ceaa2b2cd2a511b07dea24ee4ca6203e37ead
lemiskutswa <lemiskutswa20@gmail.com>
2024-11-21 14:11:57 +0300

    add save to local storage feature

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const counterElement = document.getElementById("counter-element").innerText = 12;
+let count = 0;
+
+function incrementCount() {
+    count++;
+    document.getElementById("counter-element").innerHTML = count;
+    console.log(count);
+}
