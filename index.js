b42d8600c3ec1255fc1271b111e15a7943146e5b
lemiskutswa <lemiskutswa20@gmail.com>
2024-12-12 20:29:05 +0300

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 let count = 0;
+let countElement = document.getElementById("counter-element"); 
 
 function incrementCount() {
     count++;
-    document.getElementById("counter-element").innerHTML = count;
-    console.log(count);
+    countElement.innerHTML = count;
 }
+
+let saveElement = document.getElementById("save-el");
+
+function save() {
+    let countStr = count + " - "
+    saveElement.textContent += countStr
+    countElement.textContent = 0;
+    count = 0;
+}
+
+
+
+
